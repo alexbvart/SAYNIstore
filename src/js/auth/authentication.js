b2637bb63d2ae2ca4c1689a5a62f4e64becafe3a
Alexander Briones <alexanderbvart@gmail.com>
2020-11-17 07:55:26 -0500

    login google and facebook

--- src/js/auth/authentication.js
+++ src/js/auth/authentication.js
@@ -1,8 +1,8 @@
 console.log("authentication");
 /* class Authentication{ */
 
-    avatarImg = document.getElementById('avatarImg');
-    displayName = document.getElementById('displayName');
+var avatarImg = document.getElementById('avatarImg');
+var displayName = document.getElementById('displayName');
     
 /*     function startWithGoogle(){
         const provider = new firebaseConfig.authDomain.GoogleAuthProvider()
@@ -34,7 +34,7 @@ swgoogle.addEventListener('click',()=>{
         .then(result => {
             avatarImg.attr('src',result.user.photoURL)
             avatarImg.attr('alt',result.user.displayName)
-            displayName.innerHTML="{result.user.displayName}"
+            displayName.innerHTML=" jose ${result.user.displayName}"
         })
         .catch(error=>{
             console.error(error);
@@ -43,5 +43,42 @@ swgoogle.addEventListener('click',()=>{
 });
 
 swfacebook.addEventListener('click',()=>{
-    const provider = n
+    const provider = new firebase.auth.FacebookAuthProvider();
+
+    auth.signInWithPopup(provider)
+    .then(result => {
+        avatarImg.attr('src',result.user.photoURL)
+        avatarImg.attr('alt',result.user.displayName)
+        displayName.innerHTML="{result.user.displayName}"
+    })
+    .catch(error=>{
+        console.error(error);
+    })
 });
+
+
+
+
+
+auth.onAuthStateChanged(user => {
+
+    
+    if (user) {
+        console.log('sign in ... dentro');
+        loginCheck(user);
+        headerNav.innerHTML=
+        `
+        <img 
+                class="circular-image"
+                id="avatarImg"
+                src="https://lh3.googleusercontent.com/a-/AOh14Gj7w-zVx8L6cQB3L6j8Rpe7YkyZOaI-RcruLNGi=s96-c" 
+                alt="foto de perfil">
+        `;
+/*         btnOpenPopup.innerHTML="Ingresa" */
+
+    } else {
+        console.log('sign out ... fuera');   
+        
+        loginCheck(user);
+    }
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,28 @@
-console.log("main");
+/* console.log("main"); */
+/* nav__Header */
+
+
+const  headerNav= document.querySelector('.header__nav');
+
+
+
+/* Links */
+const  outLinks= document.querySelectorAll('.logged-out');
+const  inLinks = document.querySelectorAll('.logged-in');
+console.log(outLinks);
+console.log(inLinks);
+
+const loginCheck = user => {
+    if (user) {
+        inLinks.forEach(link=> link.style.display='block');
+        outLinks.forEach(link=> link.style.display='none');
+    } else {
+        inLinks.forEach(link=> link.style.display='none');
+        outLinks.forEach(link=> link.style.display='block');
+    }
+}
+
+
 
 
 
@@ -35,6 +59,10 @@ const setupCategories = data =>{
 }
 
 
+/* Cargar foto de perfil y nombre de usuario */
+
+
+
 
 /* eventos */
 /* Mostrar los datos solo si el usuario esta logeado */
@@ -49,9 +77,26 @@ auth.onAuthStateChanged(user => {
     })
     
     if (user) {
-        console.log('sign in');
+        console.log('sign in ... dentro');
+        loginCheck(user);
+        headerNav.innerHTML=
+        `
+        <img 
+                class="circular-image"
+                id="avatarImg"
+                src="https://lh3.googleusercontent.com/a-/AOh14Gj7w-zVx8L6cQB3L6j8Rpe7YkyZOaI-RcruLNGi=s96-c" 
+                alt="foto de perfil">
+        `;
+/*         btnOpenPopup.innerHTML="Ingresa" */
 
     } else {
-        console.log('sign out');   
+        console.log('sign out ... fuera');   
+        
+        loginCheck(user);
     }
 })
+
+
+
+/* PopUp */
+/* const btnOpenPopup = document.getElementById('btn-open-popup'); */
